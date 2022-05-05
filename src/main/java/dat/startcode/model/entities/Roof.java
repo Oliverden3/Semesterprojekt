78f7991df95ab1b377c667dd6e38bacdb90a1277
Oliverden3 <oliversjensen@hotmail.com>
2022-05-05 10:36:07 +0200

    Created entities

--- src/main/java/dat/startcode/model/entities/Roof.java
+++ src/main/java/dat/startcode/model/entities/Roof.java
@@ -3,4 +3,18 @@ package dat.startcode.model.entities;
 public class Roof {
     String roofType;
     int roofTilt;
+
+    public Roof(String roofType, int roofTilt) {
+        this.roofType = roofType;
+        this.roofTilt = roofTilt;
+    }
+
+    public String getRoofType() {
+        return roofType;
+    }
+
+    public int getRoofTilt() {
+        return roofTilt;
+    }
 }
+
--- src/main/java/dat/startcode/model/entities/Toolshed.java
+++ src/main/java/dat/startcode/model/entities/Toolshed.java
@@ -3,4 +3,17 @@ package dat.startcode.model.entities;
 public class Toolshed {
     int width;
     int length;
+
+    public Toolshed(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
