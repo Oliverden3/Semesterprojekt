cef5c0ba2c806d8d395f7c06555c048657e04656
jonathanbraad <jonathanbraad123@gmail.com>
2022-05-18 13:17:27 +0200

    JSP INDEX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import dat.startcode.model.entities.Carport;
+import dat.startcode.model.entities.Order;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.CarportMapper;
 import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.persistence.OrderMapper;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -10,11 +17,15 @@ public class Main {
     public static void main(String[] args) throws DatabaseException {
         ConnectionPool connectionPool = new ConnectionPool();
         CarportMapper carportMapper = new CarportMapper(connectionPool);
+        OrderMapper orderMapper = new OrderMapper(connectionPool);
 
-        List<Carport> carportList = carportMapper.getCarport();
-        for (Carport carport:carportList) {
-            System.out.println(carport);
+        List<Order> orderList = orderMapper.getOrders();
+        for (Order order:orderList) {
+            System.out.println(order);
         }
+
+
+        orderMapper.createOrder(5, "18-05-2022", 2, 2, 3);
 
     }
 }
--- src/main/java/dat/startcode/model/entities/Order.java
+++ src/main/java/dat/startcode/model/entities/Order.java
@@ -1,11 +1,61 @@
 package dat.startcode.model.entities;
 
+import java.sql.Time;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Order {
-    Date date;
-    User user;
-    Carport carport;
-    Partslist partslist;
+    int idOrders;
+    String date;
+    int userId;
+    int width;
+    int length;
 
+    public Order(int idOrders, String date, int userId, int width, int length) {
+        this.idOrders = idOrders;
+        this.date = date;
+        this.userId = userId;
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getIdOrders() {
+        return idOrders;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setIdOrders(int idOrders) {
+        this.idOrders = idOrders;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
